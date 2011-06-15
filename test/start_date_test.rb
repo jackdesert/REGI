355b0baaefd9c253c540b00429ce6c25b8e60e36
Jack Desert <jd@letseatalready.com>
2011-06-14 20:14:40 -0400

    added a couple assertions

--- test/start_date_test.rb
+++ test/start_date_test.rb
@@ -91,8 +91,12 @@ class AMC < Test::Unit::TestCase
         @truck.b.button(:value, "Update Event").click
         assert(@truck.b.text.include?( "This event has been updated in the database"), "Event not inserted")
         assert(@truck.b.html.include?( new_date ),"Start date not found on page")
+        end_date = "2011-02-30"
+        @truck.b.text_field(:name, "end_date").value = end_date
+        @truck.b.button(:value, "Update Event").click
+        assert(@truck.b.text.include?( "Invalid Combination"), "Javascript was supposed to catch bad date combo")
 
-        @truck.b.close
+        #@truck.b.close
 
     end
 
