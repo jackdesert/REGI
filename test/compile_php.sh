1726838fd4b01d72af9f0a472e0cccf343b04302
Jack Desert <jd@letseatalready.com>
2011-06-16 10:26:16 -0400

    Figured out how to get php files to compile in batch, with errors saved to a file

--- test/compile_php.sh
+++ test/compile_php.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+echo "Beginning\n\n" > results.txt
 for file in ../regi/*
 do
     echo "$file"
+    php $file 2>> results.txt
 done
+
+echo
+echo "******************************************************************"
+echo "          ANY ERRORS WILL BE BELOW THIS LINE"
+echo
+echo
+echo
+cat results.txt | grep "PHP Parse error"
+
