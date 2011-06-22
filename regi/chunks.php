94d5bf558c6fe24abb1342123829505a3e76545f
Jack Desert <JackDesert556@gmail.com>
2011-06-22 02:20:48 -0400

    changed regi rev number for this branch to 0.51

--- regi/chunks.php
+++ regi/chunks.php
@@ -81,7 +81,7 @@ function CHUNKstartcontent($user='', $event='', $tab=''){
 }
 
 function CHUNKfinishcontent(){
-    print "<br><br></div></div></div><div id='footer'>Copyright 2011 &nbsp;&nbsp;Powered by REGI Trunk</div>";
+    print "<br><br></div></div></div><div id='footer'>Copyright 2011 &nbsp;&nbsp;Powered by REGI 0.51</div>";
 }
 
 function CHUNKlefttabs($user, $event, $tab){
