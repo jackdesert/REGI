5573f11f27684ecb26dfc2d9e3f7cd507c759a21
Jack Desert <JackDesert556@gmail.com>
2011-06-28 10:07:25 -0400

    This is the first commit of branch 0.50 changed ORDER BY to reverse for myTrips This reflects what's in production as of May 28 2011.

--- regi/myTrips.php
+++ regi/myTrips.php
@@ -75,7 +75,7 @@
                 FROM events, user_events
                 WHERE events.event_id=user_events.event_id
                 AND user_events.user_id=$my_user_id
-                ORDER BY register_status ASC
+                ORDER BY register_date DESC
                 LIMIT 30;";
 
         $result = mysql_query($query);
