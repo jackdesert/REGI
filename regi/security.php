b8e9defb9d487bedd1577bc5099303537282a81a
Jack Desert <JackDesert556@gmail.com>
2011-07-11 16:54:54 -0400

    three assertions passing

--- regi/security.php
+++ regi/security.php
@@ -19,8 +19,33 @@
 
 
 function SECcreateCookie($user_name, $special ){
-    $HMAC = 'byebye';
-    return 'AMCuser_name=' . $user_name . $HMAC;
+    $cookiePayload = 'AMCuser_name=' . $user_name;
+    $hmac_key = hash_hmac('sha1',$cookiePayload, $special);
+    $delimiter = '&';
+    $cookie = $cookiePayload . $delimiter . $hmac_key;
+    return $cookie;
+}
+
+function SECtestCookie($in_cookie, $special){
+    $delimiter = '&';
+    $parts = explode($delimiter, $in_cookie, 2);
+    if (count($parts) != 2){
+        print 'should have split into exactly two pieces';
+        return false;   // should be exactly one delimiter in the in_cookie
+    }elseif(strlen($parts[0]) < 14){
+        print 'not long enough';
+        return false;   // not long enough to contain the 'AMCuser_name' prepend
+    }else{
+        $cookiePayload = $parts[0];
+        $proposed_user_name = substr($cookiePayload, 13);
+        $in_hmac_key = $parts[1];
+        $generated_hmac_key = hash_hmac('sha1',$cookiePayload, $special);
+        if ($in_hmac_key == $generated_hmac_key)
+            return $proposed_user_name;
+        else
+            print 'hmac key did not match';
+            return false;
+    }
 }
 ?>
 
--- test/unit_tests/SECURITY_unit_tests.php
+++ test/unit_tests/SECURITY_unit_tests.php
@@ -14,8 +14,15 @@ class UTILtest extends PHPUnit_Framework_TestCase
         $this->assertGreaterThan(5, strlen($myCookie), 'cookie less than 5 characters');
         $found = strstr($myCookie, 'user_name=');
         $this->assertNotEquals(false, $found, 'string user_name= not found in cookie');
-
+        // Now test a bogus cookie
+        $bogusCookie = 'sjdiekelsickeh';
+        $badresult = SECtestCookie($bogusCookie, $this->special_code);
+        $this->assertEquals(false, $badresult, 'bad cookie accepted');
+        // Now test $myCookie, which should prove to be a valid cookie
+        $goodresult = SECtestCookie($myCookie, $this->special_code);
+        $this->assertEquals($cookieData, $goodresult, 'good cookie not accepted');
     }
+
 
 }
 ?>
