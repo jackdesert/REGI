a2276c6cce011c7186bf6a39892e03965f6c45ac
Jack Desert <JackDesert556@gmail.com>
2011-07-29 11:32:21 -0400

    removed reference to Sfirst_name in logout.php

--- regi/logout.php
+++ regi/logout.php
@@ -2,8 +2,6 @@
 
     include 'utils.php';
     session_start();
-    $_SESSION['Smessage'] = "Good bye, $_SESSION[Sfirst_name], you are now logged off.";
-    //UTILdbconnect();
 
     $_SESSION = array();    // This removes all session vars.
     session_destroy();
