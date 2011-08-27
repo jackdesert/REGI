1f0a7310a6765a05c2f9c1f439973539b4b4801b
Jack Desert <JackDesert556@gmail.com>
2011-08-27 17:07:27 -0400

    emails are almost ready. all admins are tested

--- regi/emails.php
+++ regi/emails.php
@@ -17,21 +17,18 @@
     <http://www.gnu.org/licenses/>.
 */
 function leader_means(){
-    $means = "You will receive an email any time someone registers for this event. ";
-    $means += "From the Roster tab, you can approve other registrants, export an Excel spreadsheet of registrants, and send emails to approved participants. ";
-    $means += "From the Admin tab, you can update the event details. ";
+    $means = "\n\nYou (along with all the other leaders, coleaders, and registrars of this event) will receive an email any time someone registers for this event. ";
+    $means .= "You can also administer this trip as follows: From the Roster tab, you can approve other registrants, export an Excel spreadsheet of registrants, and send emails to approved participants. ";
+    $means .= "From the Admin tab, you can update the event details. ";
     return $means;
 }
-function admin_also_means(){
-    $also = "As and admin, you will not be participating in the event. But you get all the other perks of being a leader. ";
+function registrar_also_means(){
+    $also = "\n\nAs a registrar, you will not be participating in the event. But you get all the other perks of being a leader. ";
     return $also;
 }
 
 function congrats_admin($reg_status, $event_name){
-    $article = "a";
-    if ($reg_status == admin)
-        $article = "an";
-    $congrats = "Congratulations! You are now listed as $article $reg_status for the following event: $event_name. ";
+    $congrats = "Congratulations! You are now listed as a $reg_status for the following event: $event_name. ";
     return $congrats;
 }
 function regi_link($event_id){
@@ -43,42 +40,55 @@ function regi_link($event_id){
 function waitlist_means(){
     $wait = "Waitlist means...";
 }
+function contact_leader(){
+    return "\n\nPlease contact the event leader if you have any questions.";
+}
 
+function thank($reg_status){
+    return "\n\nThank you for being a $reg_status!\n\n";
+}
 function reg_status_email($first_name, $reg_status, $event_name, $event_id){
     $reg_status = strtolower($reg_status);
     $bit = "Hello $first_name,\n\n";
     switch($reg_status) {
         case "leader":
-            $bit += congrats_admin($reg_status, $event_name);
-            $bit += regi_link($event_id);
+            $bit .= congrats_admin($reg_status, $event_name);
+            $bit .= regi_link($event_id);
+            $bit .= leader_means();
+            $bit .= thank($reg_status);
         break;
         case "coleader":
-            $bit += congrats_admin($reg_status, $event_name);
-            $bit += regi_link($event_id);
+            $bit .= congrats_admin($reg_status, $event_name);
+            $bit .= regi_link($event_id);
+            $bit .= leader_means();
+            $bit .= thank($reg_status);
         break;
         case "registrar":
-            $bit += congrats_admin($reg_status, $event_name);
-            $bit += regi_link($event_id);
+            $bit .= congrats_admin($reg_status, $event_name);
+            $bit .= regi_link($event_id);
+            $bit .= registrar_also_means();
+            $bit .= leader_means();
+            $bit .= thank($reg_status);
         break;
         case "approved":
-            $bit += "Congratulations! You have been approved to participate in the following event: $event_name. ";
-            $bit += regi_link($event_id);
+            $bit .= "Congratulations! You have been approved to participate in the following event: $event_name. ";
+            $bit .= regi_link($event_id);
+            $bit .= contact_leader();
         break;
         case "waitlist":
-            $bit += "You are on the wait list for the following event: $event_name. ";
-            $bit += waitlist_means();
-            $bit += regi_link($event_id);
+            $bit .= "You are on the wait list for the following event: $event_name. ";
+            $bit .= waitlist_means();
+            $bit .= regi_link($event_id);
+            $bit .= contact_leader();
         break;
         case "canceled":
-            $bit += "You are no longer registered for the folling event: $event_name.\n\n";
-            $bit += "If you believe this is a mistake, please contact the event leader.\n\n";
+            $bit .= "You are no longer registered for the folling event: $event_name.\n\n";
+            $bit .= "If you believe this is a mistake, please contact the event leader.\n\n";
         break;
 
 
     }
-    $bit += "Please contact the event leader if you have any questions.\n\nThank you!";
-    print $bit;
-    return 'wonder';
+    return $bit;
 }
 
 /*Note there is purposefully no closing php tag here, because
