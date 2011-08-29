cc439d30411e40cd055c0c2b03ee1e55cf01a736
Jack Desert <JackDesert556@gmail.com>
2011-08-29 08:16:18 -0400

    added a line for julie/abby's emails

--- regi/test_mailer.php
+++ regi/test_mailer.php
@@ -2,15 +2,17 @@
 include 'utils.php';
 
 
-$to = 'jackdesert556@gmail.com';
-$event_name = "Beer and Wine Fest";
-$statuses = Array('CANCELED', 'SUBMITTED', 'WAITLIST', 'APPROVED', 'REGISTRAR', 'COLEADER', 'LEADER');
+//$to = "Julie LePage <julielepage88@gmail.com>, Abby Driscoll <atdriscoll@gmail.com>, Jack Desert <jackdesert556@gmail.com>" ;
+$to = "Jack Desert <jackdesert556@gmail.com>" ;
+$first_name = "Tina";
+$event_name = "Fall Hike to Trail's Cabin";
+$statuses = Array('CANCELED', 'WAITLIST', 'APPROVED', 'REGISTRAR', 'COLEADER', 'LEADER');
 
 $event_id = 100;
 foreach ($statuses as $index => $reg_status){
 
     $title="AMC REGI Status: $reg_status -- $event_name";
-    $message = reg_status_email('first_name', $reg_status, $event_name, $event_id);
+    $message = reg_status_email($first_name, $reg_status, $event_name, $event_id);
     print $title . "\n\n";
     print $message;
     print "\n\n====================================================\n\n";
