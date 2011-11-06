e4dda43e46e5aaec6a88207cbf1527df96495e74
Jack Desert <JackDesert556@gmail.com>
2011-11-06 16:36:33 -0500

    no more reporting strict errors

--- regi/log/report.sh
+++ regi/log/report.sh
@@ -1,10 +1,9 @@
+# See if there are any new errors
+diff /home/hbboston/public_html/regi/log/php_errors.log /home/hbboston/public_html/regi/log/php_errors_prev.log > /home/hbboston/public_html/regi/log/php_errors_diff.log
+cp /home/hbboston/public_html/regi/log/php_errors.log /home/hbboston/public_html/regi/log/php_errors_prev.log
+
+
 # Check if there were any errors, and if so, mail them
-if [ -s /home/hbboston/public_html/regi/log/php_errors.log ]
-    then cat /home/hbboston/public_html/regi/log/php_errors.log | mail -s "PHP Errors in php_errors.log" jworky@gmail.com
+if [ -s /home/hbboston/public_html/regi/log/php_errors_diff.log ]
+    then cat /home/hbboston/public_html/regi/log/php_errors_diff.log | mail -s "New PHP Errors in php_errors.log" jworky@gmail.com
 fi
-
-
-if [ -s /home/hbboston/public_html/regi/log/errorlog.log ]
-    then cat /home/hbboston/public_html/regi/log/errorlog.log | mail -s "MySQL Errors in errorlog.log" jworky@gmail.com
-fi
-
