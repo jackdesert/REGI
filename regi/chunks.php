86275018346f111a24707d6193b614ddf571332b
Jack Desert <JackDesert556@gmail.com>
2012-04-21 12:21:07 -0400

    updated footer to include hyperlink to source code

--- regi/chunks.php
+++ regi/chunks.php
@@ -131,7 +131,7 @@ function CHUNKstartcontent($user='', $event='', $tab='none'){
 }
 
 function CHUNKfinishcontent(){
-    print "<br><br></div></div></div><div id='footer'>Copyright 2011 &nbsp;&nbsp;Powered by REGI-0.55d</div></body></html>";
+    print "<br><br></div></div></div><div id='footer'>Copyright 2011 &nbsp;&nbsp;Powered by <a style='color: #ccc; text-decoration: underline;' href='https://github.com/jackdesert/REGI'>REGI-0.55d</a></div></body></html>";
 }
 
 function CHUNKlefttabs($user, $event, $tab){
